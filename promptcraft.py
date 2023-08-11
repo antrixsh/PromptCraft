7eb1169151cea48b988e3cee3b9758fae3ef99a8
Antrixsh Gupta <antrixsh@gmail.com>
2023-08-11 14:59:28 +0530

    web_commit

--- promptcraft.py
+++ promptcraft.py
@@ -1,17 +1,18 @@
+from flask import Flask, render_template, request
 from models.model_handler import select_model, configure_model
 from utils.prompt_generator import generate_prompt
 
-def main():
-    print("Welcome to PromptCraft!")
-    
-    user_input = input("Enter your question or prompt: ")
-    selected_model = select_model()
-    model_config = configure_model(selected_model)
-    
-    generated_prompt = generate_prompt(user_input, model_config)
-    
-    print("Generated Prompt:")
-    print(generated_prompt)
-    
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        selected_model = select_model()
+        model_config = configure_model(selected_model)
+        generated_prompt = generate_prompt(user_input, model_config)
+        return render_template("index.html", user_input=user_input, generated_prompt=generated_prompt)
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
